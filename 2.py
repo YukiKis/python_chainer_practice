b97c368fbab5b6b82d316127f4283475f2382546
Yuki-K <s143068@gmail.com>
2020-12-04 15:26:27 +0900

    finish chapter2

---
+++ 2.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a = 1
+a
+
+print(a)
+
+# print(a)
+
+a = 1
+b = 1.2
+c = "Chainer"
+
+type(a)
+type(b)
+type(c)
+
+type(0)
+type(0.)
+type(.0)
+
+type(5.0)
+type(5.)
+type(.5)
+
+a, b = 1, 1.2
+
+a, b, c = 1, 1.2, "Chainer"
+
+str(1) + "番目"
+count = 0
+count += 1
+
+print("Hello\nWorld")
+print("Hello\tWorld")
+
+name = "Chainer"
+name.lower()
+name.upper()
+
+"{} チュートリアルへようこそ".format(name)
+name1 = "Chainer"
+name2 = "チュートリアル"
+"{} {}へようこそ".format(name1, name2)
+
+0.5.as_integer_ratio()
+0.25.as_integer_ratio()
+
